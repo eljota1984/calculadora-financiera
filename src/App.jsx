6f309f5d54568d3be0c4aa31f49917f9eb982473
Juan Pablo Orellana <juan.orellana198@gmail.com>
2026-05-10 18:20:13 -0400

    Crear proyecto React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="wrapper">
+      <header>
+        <div className="badge">💸 Herramienta Personal</div>
+        <h1>
+          Calculadora de
+          <br />
+          <span>Carga Financiera</span>
+        </h1>
+        <p className="subtitle">
+          Registra tus ingresos y gastos para conocer tu capacidad de endeudamiento y ahorro mensual.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
